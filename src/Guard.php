6fed3bb99b5a44bfbde3f30320d17bfc3d3dd8c1
odan <d.opitz@outlook.com>
2019-03-11 23:44:06 +0100

    Added support for ajax requests with x-csrf header

--- src/Guard.php
+++ src/Guard.php
@@ -144,10 +144,16 @@ class Guard
 
         // Validate POST, PUT, DELETE, PATCH requests
         if (in_array($request->getMethod(), ['POST', 'PUT', 'DELETE', 'PATCH'])) {
-            $body = $request->getParsedBody();
-            $body = $body ? (array)$body : [];
-            $name = isset($body[$this->prefix . '_name']) ? $body[$this->prefix . '_name'] : false;
-            $value = isset($body[$this->prefix . '_value']) ? $body[$this->prefix . '_value'] : false;
+            $name = $request->getHeaderLine('X-CSRF-NAME');
+            $value = $request->getHeaderLine('X-CSRF-VALUE');
+
+            if (!$name || !$value) {
+                $body = $request->getParsedBody();
+                $body = $body ? (array)$body : [];
+                $name = isset($body[$this->prefix . '_name']) ? $body[$this->prefix . '_name'] : false;
+                $value = isset($body[$this->prefix . '_value']) ? $body[$this->prefix . '_value'] : false;
+            }
+
             if (!$name || !$value || !$this->validateToken($name, $value)) {
                 // Need to regenerate a new token, as the validateToken removed the current one.
                 $request = $this->generateNewToken($request);
